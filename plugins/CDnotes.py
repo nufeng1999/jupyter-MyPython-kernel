6c9e9803db2dbde066b2ca663359b7569bc26ac8
nufeng1999 <18478162@21cn.com>
2021-12-25 16:47:33 +0800

    add mdf lable

--- plugins/CDnotes.py
+++ plugins/CDnotes.py
@@ -1,39 +1,40 @@
-## %file:src/overwritefile.py
+## %file:src/CDnotes.py
 from typing import Dict, Tuple, Sequence,List
 from plugins.ISpecialID import IStag,IDtag,IBtag,ITag
-import os
-class MyOverwritefile(IBtag):
+from jupyter_MyC_kernel.kernel import CKernel
+class MyCDnotes(IDtag):
     kobj=None
     def getName(self) -> str:
         # self.kobj._write_to_stdout("setKernelobj setKernelobj setKernelobj\n")
         
-        return 'MyOverwritefile'
+        return 'MyCDnotes'
     def getAuthor(self) -> str:
         return 'Author'
     def getIntroduction(self) -> str:
-        return 'MyOverwritefile'
+        return 'MyCDnotes'
     def getPriority(self)->int:
         return 0
     def getExcludeID(self)->List[str]:
         return []
-    def getIDBptag(self) -> List[str]:
-        return ['overwritefile']
-    def setKernelobj(self,obj):
+    def getIDDpbegintag(self) -> List[str]:
+        return ['/*']
+    def getIDDpendtag(self) -> List[str]:
+        return ['*/']
+    def setKernelobj(self,obj:CKernel):
         self.kobj=obj
         # self.kobj._write_to_stdout("setKernelobj setKernelobj setKernelobj\n")
         return
     def on_shutdown(self, restart):
         return
-    def on_IBpCodescanning(self,magics,line) -> str:
-        # self.kobj._write_to_stdout(line+" on_ISpCodescanning\n")
-        self.kobj.addkey2dict(magics,'overwritefile')
-        magics['overwritefile'] = ['true']
-        return ''
+    def on_IDpReorgCode(self,magics,line) -> str:
+        # self.kobj._write_to_stdout(line+" hon_IDpReorgCode\n")
+        #  self.addkey2dict(magics,'dartcmd')
+        return self.cleancqm(self,line)
+        # return ''
     ##在代码预处理前扫描代码时调用     
     def on_Codescanning(self,magics,code)->Tuple[bool,str]:
         pass
         return False,code
-    ##生成文件时调用
     def on_before_buildfile(self,code,magics)->Tuple[bool,str]:
         return False,''
     def on_after_buildfile(self,returncode,srcfile,magics)->bool:
@@ -48,5 +49,26 @@ class MyOverwritefile(IBtag):
         return False
     def on_after_completion(self,returncode,execfile,magics)->bool:
         return False
-
-    
+    def _is_cqm_begin(self,line):
+        if line==None or line=='':return ''
+        return line.lstrip().startswith('/*')
+    def _is_cqm_end(self,line):
+        if line==None or line=='':return ''
+        return line.rstrip().endswith('*/')
+    iscqm=False
+    def cleancqm(self,line):
+        # self.kobj._write_to_stdout(line+"\n")
+        if not self.iscqm:
+            istb=self._is_cqm_begin(self,line)
+            if istb: 
+                self.iscqm=True
+                if len(line.strip())>5:
+                    iste=self._is_cqm_end(self,line)
+                    if iste:self.iscqm=False
+                return ''
+        iste=self._is_cqm_end(self,line)
+        if iste:
+            self.iscqm=False
+            return ''
+        line= "" if self.iscqm else line
+        return line
